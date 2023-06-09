ed68edfe42cb93286112e654a265c228a4e1dd79
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-09 16:02:13 +0100

    Created DTOs

---
+++ Play.Catalogue/src/Play.Catalogue.Service/DTOs.cs
@@ -0,0 +1,6 @@
+namespace Play.Catalogue.Service.DTOs
+{
+    public record ItemDTO(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+    public record CreateItemDTO(string Name, string Description, decimal Price);
+    public record UpdateItemDTO(string Name, string Description, decimal Price);
+}
