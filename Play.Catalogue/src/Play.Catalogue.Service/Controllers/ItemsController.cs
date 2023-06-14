f45bca785cc01164b09d7382aee31a528e72954c
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-14 14:01:17 +0100

    Set up controller with get/post/put/delete

---
+++ Play.Catalogue/src/Play.Catalogue.Service/Controllers/ItemsController.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Mvc;
+using Play.Catalogue.Service.DTOs;
+
+namespace Play.Catalogue.Service.Controllers
+{
+
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        public static readonly List<ItemDTO> Items = new()
+        {
+            new ItemDTO(Guid.NewGuid(), "Potion", "Restores a small amount of hp", 5, DateTimeOffset.UtcNow),
+            new ItemDTO(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
+            new ItemDTO(Guid.NewGuid(), "Bronze sword", "Deals a small amount of damage", 10, DateTimeOffset.UtcNow)
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDTO> Get()
+        {
+            return Items;
+        }
+
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public ItemDTO GetById(Guid id)
+        {
+            var item = Items.Where(item => item.Id == id).SingleOrDefault();
+            return item;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDTO> Post(CreateItemDTO createItemDTO)
+        {
+            var item = new ItemDTO(Guid.NewGuid(), createItemDTO.Name, createItemDTO.Description, createItemDTO.Price, DateTimeOffset.UtcNow);
+            Items.Add(item);
+            // CreatedAtAction returns a 201 status and location header to retrieve the created item
+            // the location should match a GET request to return the same item
+            return CreatedAtAction(nameof(GetById), new { IdentityServiceCollectionExtensions = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, UpdateItemDTO updateItemDTO)
+        {
+            var existingItem = Items.Where(ItemDTO => ItemDTO.Id == id).SingleOrDefault();
+            var updatedItem = existingItem with
+            {
+                Name = updateItemDTO.Name,
+                Description = updateItemDTO.Description,
+                Price = updateItemDTO.Price
+            };
+
+            var index = Items.FindIndex(item => existingItem.Id == id);
+            Items[index] = updatedItem;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var index = Items.FindIndex(item => item.Id == id);
+            Items.RemoveAt(index);
+
+            return NoContent();
+        }
+    }
+}
