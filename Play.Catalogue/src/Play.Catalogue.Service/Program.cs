d2ed7eb2395b1ce61a3f411ff8acdc1260db6881
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-14 15:49:13 +0100

    Suppress async suffix in action names set to false

--- Play.Catalogue/src/Play.Catalogue.Service/Program.cs
+++ Play.Catalogue/src/Play.Catalogue.Service/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMvc(options => { options.SuppressAsyncSuffixInActionNames = false; });
+builder.Services.AddControllers(options => options.SuppressAsyncSuffixInActionNames = false);
 
 var app = builder.Build();
 
