eb3b96405ef7119be7e6a7a7327e9a73adee1b42
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-14 14:12:39 +0100

    Added validation for name and price fields.

--- Play.Catalogue/src/Play.Catalogue.Service/DTOs.cs
+++ Play.Catalogue/src/Play.Catalogue.Service/DTOs.cs
@@ -1,6 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Play.Catalogue.Service.DTOs
 {
     public record ItemDTO(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
-    public record CreateItemDTO(string Name, string Description, decimal Price);
-    public record UpdateItemDTO(string Name, string Description, decimal Price);
+    public record CreateItemDTO([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
+    public record UpdateItemDTO([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
 }
