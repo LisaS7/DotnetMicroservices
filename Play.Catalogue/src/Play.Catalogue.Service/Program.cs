2ff2348d990e0dea00f8d084d79d35037e1728a6
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-16 14:45:11 +0100

    Refactored settings to simplify program.cs

--- Play.Catalogue/src/Play.Catalogue.Service/Program.cs
+++ Play.Catalogue/src/Play.Catalogue.Service/Program.cs
@@ -1,33 +1,10 @@
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Driver;
 using Play.Catalogue.Service.Entities;
 using Play.Catalogue.Service.Repositories;
-using Play.Catalogue.Service.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
-// config for appsettings.json
-var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-
-// register one instance of mongodb
-builder.Services.AddSingleton(serviceProvider =>
-{
-    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-    return mongoClient.GetDatabase(serviceSettings.ServiceName);
-});
-
-builder.Services.AddSingleton<IRepository<Item>>(serviceProvider =>
-{
-    var database = serviceProvider.GetService<IMongoDatabase>();
-    return new MongoRepository<Item>(database, "items");
-});
+builder.Services.AddMongo().AddMongoRepository<Item>("items");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
